322f70b8e1fedba074384aedce50e5a395d01c85
linda <lindawilson.may21@gmail.com>
2023-09-22 12:22:45 +0200

    background_job in controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,18 @@
 class UsersController < ApplicationController
+
+  def update
+    if current_user.update(user_params)
+      FakeJob.perform_later(current_user)  # <- The job is queued
+      flash[:notice] = "Your profile has been updated"
+      redirect_to root_path
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password, :admin)
+  end
 end
